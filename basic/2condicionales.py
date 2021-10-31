02952f7acd93145a0b6e12d8d6f9aa8798069c77
Gael Ramos <esgaelramos@gmail.com>
2021-10-30 20:13:36 -0600

    I want this commit.

---
+++ basic/2condicionales.py
@@ -0,0 +1,24 @@
+# edad = int(input("Escribe tu edad: "))
+# if edad > 17:
+#     print("Eres mayor de edad")
+# else:
+#     print("Eres menor de edad")
+
+# #Se rompe si se pone un numero con punto flotante
+# numero = int(input("Escribe un numero: "))
+# if numero > 5:
+#     print("Es mayor a 5")
+# elif numero == 5:
+#     print("Es igual a 5")
+# else:
+#     print("Es menor a 5")
+
+#Ahora no se rompe si se pone un numero con punto flotante, pero sí lo hace si no se escribe un numero
+numero = float(input("Escribe un numero: "))
+if numero > 5:
+    print("Es mayor a 5")
+elif numero == 5:
+    print("Es igual a 5")
+else:
+    print("Es menor a 5")
+
